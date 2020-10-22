afedd25922cef4a67ae641f7f2536e48ce053b40
alireza1111 <alireza.davoudian@snd.gu.se>
2020-10-22 16:06:17 +0200

    Synchronous URLValidation createds.

--- ValidateUrl/Validate.cs
+++ ValidateUrl/Validate.cs
@@ -14,44 +14,25 @@ namespace ValidateUrl
         private static readonly HttpClient Client = new HttpClient();
         public static async Task<object> IsValidUriAsync(Uri uri)
         {
-            //using HttpClient Client = new HttpClient();
             StatusCode = new StatusCode { IsValid = false };
             try
             {
-
                 HttpResponseMessage result = Task.Run( () => Client.GetAsync(uri)).Result;
                  StatusCode.Message = result.StatusCode;
             }
             catch (AggregateException ae)
             {
                 foreach (var e in ae.InnerExceptions)
-                {
-                    // Handle the custom exception.
-                    /*                    if (e is CustomException)
-                                        {*/
-                    
+                {                    
                     if (e.Message.Contains("No such host is known"))
                     {
                         StatusCode.Message = HttpStatusCode.NotFound;
                         return StatusCode;
                     }
-
-
-                    // }
-                        /*                    // Rethrow any other exception.
-                                            else
-                                            {
-                                                throw;
-                                            }*/
                 }
-                return "vaa?!";
+                StatusCode.Message = HttpStatusCode.BadRequest;
+                return StatusCode;
 
-                /*            return StatusCode switch
-                            {
-                                HttpStatusCode.Accepted => true,
-                                HttpStatusCode.OK => true,
-                                _ => false,
-                            };*/
             }
             StatusCode.IsValid = true;
             return StatusCode;
